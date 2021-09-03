7e6b14e0d71704d3d5082a94710d577fe16335be
Clemens Eichler <70262215+clemenseichler@users.noreply.github.com>
2021-09-03 08:15:59 +0200

    Update StorageServiceApplication.java

--- storage-service/src/main/java/com/github/advancedsecurity/storageservice/StorageServiceApplication.java
+++ storage-service/src/main/java/com/github/advancedsecurity/storageservice/StorageServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StorageServiceApplication {
 
-	public static final String password = "123456";
+	public static final String gh_ac_token = "30c85efc997886df0457ace44b8ae6231bca83d0";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(StorageServiceApplication.class, args);
